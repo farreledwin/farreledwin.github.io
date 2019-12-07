ae21e7ee5e0fc4878d3a48df40dd4a0bfd458aab
farreledwin <farrel206@gmail.com>
2019-12-08 02:02:24 +0700

    PWA FR

--- sw.js
+++ sw.js
@@ -3,35 +3,6 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 if (workbox) console.log(`Workbox berhasil dimuat`);
 else console.log(`Workbox gagal dimuat`);
 
-const CACHE_NAME = 'football-pwa';
-
-var urltocache = [
-	'/',
-	'/index.html',
-	'/content/home.html',
-	'/content/favoritesaya.html',
-	'/content/informasitim.html',
-	'/content/jadwaltanding.html',
-	'/css/materialize.css',
-	'/css/materialize.min.css',
-	'/css/informasitim.css',
-	'/js/idb.js',
-	'/js/indexdb.js',
-	'/manifest.json',
-	'/package-lock.json',
-	'/push.js',
-	'/sw.js',
-	'/js/materialize.js',
-	'/js/materialize.min.js',
-	'/js/nav.js',
-	'/js/api.js',
-	'/js/idb.d.ts',
-	'/js/node.js',
-	'/img/c.png',
-	'/img/soccer192.png',
-	'/img/soccer512.png',
-	'/img/notification.png'
-];
 workbox.precaching.precacheAndRoute([
 	{ url: '/', revision: '1' },
 	{ url: '/index.html', revision: '1' },
@@ -60,41 +31,24 @@ workbox.precaching.precacheAndRoute([
 	{ url: '/img/notification.png', revision: '1' }
 ]);
 
-workbox.routing.registerRoute(new RegExp('/content/'), workbox.strategies.staleWhileRevalidate());
+workbox.routing.registerRoute(
+	new RegExp('/content/'),
+	workbox.strategies.staleWhileRevalidate({ cacheName: 'content-cache' })
+);
 
-self.addEventListener('fetch', function(event) {
-	var base_url = 'https://api.football-data.org/';
-	if (event.request.url.indexOf(base_url) > -1) {
-		event.respondWith(
-			caches.open(CACHE_NAME).then(function(cache) {
-				return fetch(event.request).then(function(response) {
-					cache.put(event.request.url, response.clone());
-					return response;
-				});
+workbox.routing.registerRoute(
+	new RegExp('https://api.football-data.org/'),
+	workbox.strategies.staleWhileRevalidate({
+		cacheName: 'API-CACHE',
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxEntries: 30,
+				maxAgeSeconds: 3600
 			})
-		);
-	} else {
-		event.respondWith(
-			caches.match(event.request).then(function(response) {
-				return response || fetch(event.request);
-			})
-		);
-	}
-});
-self.addEventListener('activate', function(event) {
-	event.waitUntil(
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.map(function(cacheName) {
-					if (cacheName != CACHE_NAME) {
-						// console.log("ServiceWorker: cache " + cacheName + " dihapus");
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	);
-});
+		]
+	})
+);
+
 self.addEventListener('push', function(event) {
 	var body;
 	if (event.data) {
