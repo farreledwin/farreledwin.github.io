ab12b01e695f0cbdf40934107ea85960c44761d1
farreledwin <farrel206@gmail.com>
2020-11-04 17:54:48 +0700

    add skripsi template

---
+++ skripsi/precache-manifest.fbbbc287b0343f04c42a05daef738997.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7f9ad5dd9e706fa5df16f91c87b477",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb098d7fa0f0695221e0",
+    "url": "/static/css/2.50a5f423.chunk.css"
+  },
+  {
+    "revision": "a67f73521ac5f6a6c40e",
+    "url": "/static/css/main.2e0deaab.chunk.css"
+  },
+  {
+    "revision": "eb098d7fa0f0695221e0",
+    "url": "/static/js/2.b6cb06a4.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.b6cb06a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67f73521ac5f6a6c40e",
+    "url": "/static/js/main.8540028f.chunk.js"
+  },
+  {
+    "revision": "5d41517d38b0b7418d84",
+    "url": "/static/js/runtime-main.ff0bad79.js"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "c93e22e98b7a8d58f83ce42b278815eb",
+    "url": "/static/media/Poppins-ThinItalic.c93e22e9.ttf"
+  },
+  {
+    "revision": "7866b00d489b1ca855c5b0578ecab0fc",
+    "url": "/static/media/ardiant.7866b00d.jpg"
+  },
+  {
+    "revision": "f79abc98576e3ddf01fe0b9cf42592a9",
+    "url": "/static/media/background-2.f79abc98.jpg"
+  },
+  {
+    "revision": "56dffdc341d71b3bb3795cb4646b2724",
+    "url": "/static/media/farrel.56dffdc3.jpg"
+  },
+  {
+    "revision": "a55f0f266d38b7bc71b31b151bd837f7",
+    "url": "/static/media/vincent.a55f0f26.jpg"
+  }
+]);
